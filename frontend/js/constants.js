c333dd6deddb3cb15c6184851542933dcf426776
Mayale <maylson.alencar21@gmail.com>
2023-02-21 19:19:47 -0300

    nada

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; // Replace with your own contract address
+const contractAddress = "0x3f8129C53e97755408Da4014983a4054E465E9Aa"; // Replace with your own contract address
 const chain = 'goerli'; // goerli, polygon, or ethereum
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
