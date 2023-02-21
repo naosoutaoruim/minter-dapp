c067e33d288f7dddc9f5e4e1b55c10fe07639fbe
Mayale <maylson.alencar21@gmail.com>
2023-02-21 19:23:26 -0300

    aaaaa

--- backend/src/config.js
+++ backend/src/config.js
@@ -7,21 +7,307 @@ const { NETWORK } = require(`${basePath}/constants/network.js`);
 const network = NETWORK.eth;
 
 // General metadata for Ethereum
-const namePrefix = "YOUR COLLECTION NAME";
-const description = "Remember to replace this description";
+const namePrefix = "Crias";
+const description = "A representação da cultura das favelas no mundo da arte generativa";
 const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
 
 const layerConfigurations = [
   {
-    growEditionSizeTo: 5,
+    growEditionSizeTo: 500, //mulher preta normal
     layersOrder: [
-      { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+      { name: "fundo" },
+      { name: "preta" },
+      { name: "brinco" },
+      { name: "roupa mina" },
+      { name: "ice" },
+      { name: "cabelo mina" },
+      { name: "boca preta" },
+      { name: "olhos mina" },
+      { name: "pele" },
+      { name: "oculos" },
+      { name: "oculos na testa" },
+      { name: "mao direita preto" },
+      { name: "mao esquerda preto" },
+  
+    ],
+  },{
+    growEditionSizeTo: 1000, // mulher branca normal
+    layersOrder: [
+      { name: "fundo" },
+      { name: "branca" },
+      { name: "brinco" },
+      { name: "roupa mina" },
+      { name: "ice" },
+      { name: "cabelo mina" },
+      { name: "boca branca" },
+      { name: "olhos mina" },
+      { name: "pele" },
+      { name: "oculos na testa" },
+      { name: "oculos" },
+      { name: "mao direita branco" },
+      { name: "mao esquerda branco" },
+  
+    ],
+  },{
+    growEditionSizeTo: 1500, // mulher branca boné pra frente
+    layersOrder: [
+      { name: "fundo" },
+      { name: "branca" },
+      { name: "brinco" },
+      { name: "roupa mina" },
+      { name: "olhos mina" },
+      { name: "pele" },
+      { name: "ice" },
+      { name: "cabelo mina bone pra frente" },
+      { name: "boca branca" },
+      { name: "oculos" },
+      { name: "bone mina pra frente" },
+      { name: "oculos na testa" },
+      { name: "mao direita branco" },
+      { name: "mao esquerda branco" },
+
+    ],
+  },{
+    growEditionSizeTo: 2000, // mulher preta boné pra frente
+    layersOrder: [
+      { name: "fundo" },
+      { name: "preta" },
+      { name: "brinco" },
+      { name: "roupa mina" },
+      { name: "olhos mina" },
+      { name: "pele" },
+      { name: "ice" },
+      { name: "cabelo mina bone pra frente" },
+      { name: "boca preta" },
+      { name: "oculos" },
+      { name: "bone mina pra frente" },
+      { name: "oculos na testa" },
+      { name: "mao direita preto" },
+      { name: "mao esquerda preto" },
+
+    ],
+  },{
+    growEditionSizeTo: 2200, //mulher branca boné pra tras
+    layersOrder: [
+      { name: "fundo" },
+      { name: "branca" },
+      { name: "brinco" },
+      { name: "roupa mina" },
+      { name: "olhos mina" },
+      { name: "pele" },
+      { name: "ice" },
+      { name: "cabelo mina bone pra tras" },
+      { name: "boca branca" },
+      { name: "bone mina pra tras" },
+      { name: "oculos na testa" },
+      { name: "oculos" },
+      { name: "mao direita branco" },
+      { name: "mao esquerda branco" },
+
+    ],
+  },{
+    growEditionSizeTo: 2400, //mulher preta boné pra tras
+    layersOrder: [
+      { name: "fundo" },
+      { name: "preta" },
+      { name: "brinco" },
+      { name: "roupa mina" },
+      { name: "olhos mina" },
+      { name: "pele" },
+      { name: "ice" },
+      { name: "cabelo mina bone pra tras" },
+      { name: "boca preta" },
+      { name: "bone mina pra tras" },
+      { name: "oculos na testa" },
+      { name: "oculos" },
+      { name: "mao direita preto" },
+      { name: "mao esquerda preto" },
+  
+    ],
+  },{
+    growEditionSizeTo: 2450, // mulher branca chapéu
+    layersOrder: [
+      { name: "fundo" },
+      { name: "branca" },
+      { name: "brinco" },
+      { name: "roupa mina" },
+      { name: "olhos mina" },
+      { name: "pele" },
+      { name: "ice" },
+      { name: "chapeu mina" },
+      { name: "mao direita branco" },
+      { name: "mao esquerda branco" },
+      { name: "boca branca" },
+
+    ],
+  },{
+    growEditionSizeTo: 2500, // mulher preta chapéu
+    layersOrder: [
+      { name: "fundo" },
+      { name: "preta" },
+      { name: "brinco" },
+      { name: "roupa mina" },
+      { name: "olhos mina" },
+      { name: "pele" },
+      { name: "ice" },
+      { name: "chapeu mina" },
+      { name: "mao direita preto" },
+      { name: "mao esquerda preto" },
+      { name: "boca preta" },
+
+  
+    ],
+  },{
+    growEditionSizeTo: 3000, // homem branco normal boné pra tras
+    layersOrder: [
+      { name: "fundo" },
+      { name: "branco" },
+      { name: "brinco" },
+      { name: "roupa cria" },
+      { name: "cabelo cria" },
+      { name: "risquinho branco" },
+      { name: "boca branco" },
+      { name: "olhos cria" },
+      { name: "pele" },
+      { name: "ice" },
+      { name: "oculos" },
+      { name: "bone cria pra frente" },
+      { name: "oculos na testa" },
+      { name: "mao direita branco" },
+      { name: "mao esquerda branco" },
+
+    ],
+  },{
+    growEditionSizeTo: 3500, //homem preto normal boné pra frente
+    layersOrder: [
+      { name: "fundo" },
+      { name: "preto" },
+      { name: "brinco" },
+      { name: "roupa cria" },
+      { name: "cabelo cria" },
+      { name: "risquinho preto" },
+      { name: "boca preto" },
+      { name: "olhos cria" },
+      { name: "pele" },
+      { name: "ice" },
+      { name: "oculos" },
+      { name: "bone cria pra frente" },
+      { name: "oculos na testa" },
+      { name: "mao direita preto" },
+      { name: "mao esquerda preto" },
+  
+    ],
+  },{
+    growEditionSizeTo: 4000, // homem branco normal boné pra tras
+    layersOrder: [
+      { name: "fundo" },
+      { name: "branco" },
+      { name: "brinco" },
+      { name: "roupa cria" },
+      { name: "cabelo cria" },
+      { name: "risquinho branco" },
+      { name: "boca branco" },
+      { name: "olhos cria" },
+      { name: "pele" },
+      { name: "ice" },
+      { name: "bone cria pra tras" },
+      { name: "oculos" }, //oculos acima do bone
+      { name: "oculos na testa" }, 
+      { name: "mao direita branco" },
+      { name: "mao esquerda branco" },
+
+    ],
+  },{
+    growEditionSizeTo: 4500, //homem preto normal boné pra tras
+    layersOrder: [
+      { name: "fundo" },
+      { name: "preto" },
+      { name: "brinco" },
+      { name: "roupa cria" },
+      { name: "cabelo cria" },
+      { name: "risquinho preto" },
+      { name: "boca preto" },
+      { name: "olhos cria" },
+      { name: "pele" },
+      { name: "ice" },
+      { name: "bone cria pra tras" },
+      { name: "oculos" }, //oculos acima do bone
+      { name: "oculos na testa" },
+      { name: "mao direita preto" },
+      { name: "mao esquerda preto" },
+  
+    ],
+  },{
+    growEditionSizeTo: 4550, //homem branco chapéu
+    layersOrder: [
+      { name: "fundo" },
+      { name: "branco" },
+      { name: "brinco" },
+      { name: "roupa cria" },
+      { name: "olhos cria" },
+      { name: "pele" },
+      { name: "ice" },
+      { name: "chapeu cria" },
+      { name: "mao direita branco" },
+      { name: "mao esquerda branco" },
+      { name: "boca branco" }, // boca acima do chapeu
+
+
+    ],
+  },{
+    growEditionSizeTo: 4600, // homem preto chapeu
+    layersOrder: [
+      { name: "fundo" },
+      { name: "preto" },
+      { name: "brinco" },
+      { name: "roupa cria" },
+      { name: "olhos cria" },
+      { name: "pele" },
+      { name: "ice" },
+      { name: "chapeu cria" },
+      { name: "boca preto" }, // boca abaixo do chapeu
+      { name: "mao direita preto" },
+      { name: "mao esquerda preto" },
+
+  
+    ],
+  },{
+    growEditionSizeTo: 4800, // homem branco normal touca
+    layersOrder: [
+      { name: "fundo" },
+      { name: "branco" },
+      { name: "brinco" },
+      { name: "cabelo cria" },
+      { name: "risquinho branco" },
+      { name: "olhos cria" },
+      { name: "pele" },
+      { name: "oculos" },
+      { name: "bone cria pra frente" },
+      { name: "oculos na testa" },
+      { name: "touca cria" },
+      { name: "boca branco" }, // boca acima da touca
+      { name: "mao direita branco" },
+      { name: "mao esquerda branco" },
+
+    ],
+  },{
+    growEditionSizeTo: 5000, //homem preto normal touca
+    layersOrder: [
+      { name: "fundo" },
+      { name: "preto" },
+      { name: "brinco" },
+      { name: "cabelo cria" },
+      { name: "risquinho preto" },
+      { name: "olhos cria" },
+      { name: "pele" },
+      { name: "oculos" },
+      { name: "bone cria pra frente" },
+      { name: "oculos na testa" },
+      { name: "touca cria" },
+      { name: "boca preto" }, // boca acima da touca
+      { name: "mao direita preto" },
+      { name: "mao esquerda preto" },
+  
     ],
   },
 ];
@@ -31,13 +317,13 @@ const shuffleLayerConfigurations = true;
 const debugLogs = false;
 
 const format = {
-  width: 512,
-  height: 512,
+  width: 1000,
+  height: 1000,
   smoothing: false,
 };
 
 const extraMetadata = {
-  external_url: "https://codecats.xyz", // Replace with your website or remove this line if you do not have one.
+  external_url: "https://crias.xyz", // Replace with your website or remove this line if you do not have one.
 };
 
 // NFTPort Info
@@ -48,22 +334,22 @@ const LIMIT = 2; // Your API key rate limit
 const CHAIN = 'goerli'; // only goerli, polygon, or ethereum
 
 // REQUIRED CONTRACT DETAILS THAT CANNOT BE UPDATED LATER!
-const CONTRACT_NAME = 'CRYPTOPUNKS';
-const CONTRACT_SYMBOL = 'CP';
+const CONTRACT_NAME = 'crias.xyz';
+const CONTRACT_SYMBOL = 'CRIAS';
 const METADATA_UPDATABLE = true; // set to false if you don't want to allow metadata updates after minting
-const OWNER_ADDRESS = 'YOUR WALLET ADDRESS HERE';
-const TREASURY_ADDRESS = 'YOUR WALLET ADDRESS HERE';
+const OWNER_ADDRESS = '0x23B4aFde0906525eb35EC53A94d1D7FD87aAda2A';
+const TREASURY_ADDRESS = '0x23B4aFde0906525eb35EC53A94d1D7FD87aAda2A';
 const MAX_SUPPLY = 5000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
 const MINT_PRICE = 0.01; // Minting price per NFT. Goerli = ETH, Ethereum = ETH, Polygon = MATIC. CANNOT BE UPDATED!
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-03-20T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2023-03-13T03:00:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
-const ROYALTY_ADDRESS = "0xd8B808A887326F45B2D0cd999709Aa6264CeF919"; // Address that will receive the royalty
+const ROYALTY_ADDRESS = "0x23B4aFde0906525eb35EC53A94d1D7FD87aAda2A"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
 const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
@@ -74,8 +360,8 @@ let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually inclu
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
 const GENERIC_TITLE = CONTRACT_NAME; // Replace with what you want the generic titles to say if you want it to be different from the contract name.
-const GENERIC_DESCRIPTION = "REPLACE THIS"; // Replace with what you want the generic descriptions to say.
-const GENERIC_IMAGE = "https://ipfs.io/ipfs/QmUf9tDbkqnfHkQaMdFWSGAeXwVXWA61pFED7ypx4hcsfh"; // Replace with your generic image that will display for all NFTs pre-reveal.
+const GENERIC_DESCRIPTION = "Qual dos crias te representa?"; // Replace with what you want the generic descriptions to say.
+const GENERIC_IMAGE = "https://ipfs.io/ipfs/bafybeidl6knu64xc4ibdsvcgtk6ihgbadadp24cnc7kqrmikk5wju4ljk4"; // Replace with your generic image that will display for all NFTs pre-reveal.
 
 // Automatically set contract address if deployed using the deployContract.js script
 try {
@@ -92,12 +378,12 @@ try {
 // END NFTPort Info
 
 const solanaMetadata = {
-  symbol: "YC",
-  seller_fee_basis_points: 1000, // Define how much % you want from secondary market sales 1000 = 10%
-  external_url: "https://www.youtube.com/c/hashlipsnft",
+  symbol: "CR",
+  seller_fee_basis_points: 2000, // Define how much % you want from secondary market sales 1000 = 10%
+  external_url: "https://www.instagram.com/crias.fvl/",
   creators: [
     {
-      address: "7fXNuer5sbZtaTEPhtJ5g5gNtuyRoKkvxdjEjEnPN4mC",
+      address: "0x23B4aFde0906525eb35EC53A94d1D7FD87aAda2A",
       share: 100,
     },
   ],
