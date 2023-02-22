f88c550ef1d8ac62a09bd56c4bb5335facc9645d
Mayale <maylson.alencar21@gmail.com>
2023-02-22 19:25:04 -0300

    whitelisted

--- backend/src/config.js
+++ backend/src/config.js
@@ -352,7 +352,7 @@ const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the ro
 const ROYALTY_ADDRESS = "0x23B4aFde0906525eb35EC53A94d1D7FD87aAda2A"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = ["0x23B4aFde0906525eb35EC53A94d1D7FD87aAda2A"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
