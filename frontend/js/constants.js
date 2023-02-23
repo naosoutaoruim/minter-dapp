19b74df8c8c170d8a221ec8836735b72376ff4d1
Mayale <maylson.alencar21@gmail.com>
2023-02-22 22:44:59 -0300

    fin

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,22 +1,22 @@
 const contractAddress = "0x3f8129C53e97755408Da4014983a4054E465E9Aa"; // Replace with your own contract address
 const chain = 'goerli'; // goerli, polygon, or ethereum
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
-const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_h1 = "Bem-vindo ao projeto CRIAS";
+const welcome_h2 = "Conecte-se na Metamask para continuar";
+const welcome_p = 'Os Crias é um projeto de NFT que homenageia a cultura do Funk e das Favelas brasileiras, representando o estilo de Cria ou Chavoso. Tudo foi idealizado e produzido por @nãosoutãoruim.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
-const h1_public_mint = "Public Minting Open!";
+const h1_public_mint = "Venda Pública Aberta!";
 const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
-const h2_presale_mint = "Public Minting down";
+const h2_presale_mint = "A Venda Pública Acabou";
 const p_presale_coming_soon = "We are working hard to launch the NFT Collection. Stay tuned for updates!";
 const p_presale_mint_not_whitelisted = "You are not whitelisted for the pre-sale.. 😢";
 const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale! 🎉";
 const p_presale_mint_already_minted = "You've already claimed your whitelist mint. Thank you! 🎉";
-const p_public_mint = "No whitelist needed. Public minting is now open! 🎉";
+const p_public_mint = " A Venda Pública tá Aberta, Garnta o seu Cria!";
 const button_presale_coming_soon = "Get on the Whitelist";
 const button_presale_mint_whitelisted = "Mint Your Special NFT";
 const button_presale_mint_not_whitelisted = "Get on the Whitelist";
 const button_presale_already_minted = "Join The Community";
-const button_public_mint = "Mint Your NFT";
-const mint_failed = "Minting failed. 😢 Please try again.";
+const button_public_mint = "Garanta seu NFT dos Crias";
+const mint_failed = "Algo deu errado. 😢 Tente novamente.";
